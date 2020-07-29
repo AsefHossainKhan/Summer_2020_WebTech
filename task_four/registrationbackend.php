8b05f95ac07186dba1a3943b8ad80514d2655140
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2020-07-29 10:52:50 +0600

    finished registrationbackend.php

---
+++ task_four/registrationbackend.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    if(isset($_POST["submitButton"])) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $username = $_POST["userName"];
+        $password = $_POST["password"];
+        $gender = $_POST["gender"];
+        // $dateOfBirth = $_POST["day"] + "/" + $_POST["month"] + "/" +$_POST["year"];
+        $day = $_POST["day"];
+        $month = $_POST["month"];
+        $year = $_POST["year"];
+        //$dateOfBirth = $day + "/" + $month + "/" + $year;
+
+
+        if(empty($name) || empty($email) || empty($username) || empty($password) || empty($gender) || empty($day) || empty($month) || empty($year)) {
+            echo "empty fields found";
+        }
+        else {
+            $_SESSION["name"] = $name;
+            $_SESSION["email"] = $email;
+            $_SESSION["username"] = $username;
+            $_SESSION["password"] = $password;
+            $_SESSION["gender"] = $gender;
+            //$_SESSION["dateOfBirth"] = $dateOfBirth;
+            $_SESSION["day"] = $day;
+            $_SESSION["month"] = $month;
+            $_SESSION["year"] = $year;
+
+            //echo "Registration Successful";
+        }
+
+    }
+
+    else {
+        echo "invalid request";
+    }
+
+
+
+?>
